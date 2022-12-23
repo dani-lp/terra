41ea26ef5ad0c2186c6d7ae750626b788ec44453
Dani Lauzurica <danilau11@gmail.com>
2022-12-23 22:17:09 +0100

    Support for per-page layouts and path aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,29 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType, type AppProps } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import type { NextPage } from 'next';
+import * as React from 'react';
 
-import { trpc } from "../utils/trpc";
+import { trpc } from '../utils/trpc';
 
-import "../styles/globals.css";
+import '../styles/globals.css';
+
+export type NextPageWithLayout<P = Record<string, never>, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: React.ReactElement) => React.ReactElement;
+};
+
+type MyAppProps = AppProps & {
+  Component: NextPageWithLayout;
+  pageProps: { session: Session | null };
+};
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
-}) => {
-  return (
+}: MyAppProps) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
