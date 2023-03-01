07d68f3700e51da09eaa39b619e499c0463add49
dani-lp <danilau11@gmail.com>
2023-03-01 19:53:54 +0100

    feat: protected TRPC procedures by role

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -2,6 +2,7 @@ import { initTRPC, TRPCError } from '@trpc/server';
 import superjson from 'superjson';
 
 import { type Context } from './context';
+import { Role } from '@prisma/client';
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
@@ -37,3 +38,24 @@ const isAuthed = t.middleware(({ ctx, next }) => {
  * Protected procedure
  **/
 export const protectedProcedure = t.procedure.use(isAuthed);
+
+/**
+ * Middleware for user roles
+ */
+
+const createRoleMiddleware = (role: Role) => {
+  return t.middleware(({ ctx, next }) => {
+    if (!ctx.session || !ctx.session.user || ctx.session?.user?.role !== role) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+    return next({
+      ctx: {
+        session: { ...ctx.session, user: ctx.session.user },
+      },
+    });
+  });
+};
+
+export const playerProcedure = t.procedure.use(createRoleMiddleware(Role.PLAYER));
+export const organizationProcedure = t.procedure.use(createRoleMiddleware(Role.ORGANIZATION));
+export const adminProcedure = t.procedure.use(createRoleMiddleware(Role.ADMIN));
