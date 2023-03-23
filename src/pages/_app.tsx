754c9da3935e4c0023f6e8729e8d66ac888218ea
Dani Lauzurica <danilau11@gmail.com>
2023-03-23 21:09:34 +0100

    feat: proper favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType, type AppProps } from 'next/app';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import * as React from 'react';
 import { appWithTranslation } from 'next-i18next';
 
@@ -32,10 +33,18 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 
   return (
-    <SessionProvider session={session} refetchOnWindowFocus>
-      {layout}
-      {process.env.NODE_ENV === 'development' && <ToDevButton />}
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <SessionProvider session={session} refetchOnWindowFocus>
+        {layout}
+        {process.env.NODE_ENV === 'development' && <ToDevButton />}
+      </SessionProvider>
+    </>
   );
 };
 
