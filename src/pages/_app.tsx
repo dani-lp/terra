22dc5d9a5ea73c5a67d312ae23f9e96f659723d1
Dani Lauzurica <danilau11@gmail.com>
2023-04-17 01:01:00 +0200

    feat: redirect unauthenticated users

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import { type Session } from 'next-auth';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { appWithTranslation } from 'next-i18next';
 import { type AppProps, type AppType } from 'next/app';
 import dynamic from 'next/dynamic';
@@ -11,12 +11,29 @@ import * as React from 'react';
 import { ToDevButton } from '@/components/dev';
 import { trpc } from '../utils/trpc';
 
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 const TopProgressBar = dynamic(
   () => import('@/components/common/top-progress-bar/TopProgressBar'),
   { ssr: false },
 );
+
+/**
+ * Redirect users to the landing page if they are not authenticated
+ */
+const InternalSessionRedirect = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (!session && status === 'unauthenticated') {
+      void router.push('/');
+    }
+  }, [session, status, router]);
+
+  return null;
+};
 
 export type NextPageWithLayout<P = Record<string, never>, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: React.ReactElement) => React.ReactElement;
@@ -51,6 +68,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <SessionProvider session={session} refetchOnWindowFocus>
         {layout}
         {process.env.NODE_ENV === 'development' && <ToDevButton />}
+        <InternalSessionRedirect />
       </SessionProvider>
     </>
   );
