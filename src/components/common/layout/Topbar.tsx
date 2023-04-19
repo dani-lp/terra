1c7933cfebb349ca8eaf01a179cae1026c65033b
Dani Lauzurica <danilau11@gmail.com>
2023-04-19 23:56:56 +0200

    feat: loading skeleton for topbar nav

--- src/components/common/layout/Topbar.tsx
+++ src/components/common/layout/Topbar.tsx
@@ -10,37 +10,42 @@ import { useRouter } from 'next/router';
 import { useNavigation } from '@/components/common/layout/context/NavigationContext';
 import { NavigationButtons } from '@/components/common/layout/NavigationButtons';
 import { filterNavigationItems } from '@/components/common/layout/utils';
+import { Skeleton } from '@/components/common/skeleton';
 import { classNames } from '@/const';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const TopBarNavigation = () => {
   const { t } = useTranslation('common');
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
   const navigationItems = useNavigation();
 
   const filteredNavigationItems = filterNavigationItems(navigationItems, session?.user?.role);
 
+  const isLoading = status === 'loading';
+
   return (
     <>
-      {filteredNavigationItems.map((item) => (
-        <Link
-          href={item.to}
-          key={item.to}
-          className={classNames(
-            'flex w-full items-center justify-between gap-2 rounded-lg px-3 py-2 text-sm font-medium transition-colors duration-200',
-            router.pathname === item.to
-              ? 'bg-black text-white'
-              : 'bg-white text-black hover:bg-neutral-100',
-          )}
-        >
-          <div className="flex w-full items-center justify-center gap-2">
-            <item.icon className="w-6" />
-            {t(`sidebar.items.${item.key}`)}
-          </div>
-        </Link>
-      ))}
+      {isLoading && [...Array(3)].map((_, index) => <Skeleton key={index} className="h-8 w-28" />)}
+      {!isLoading &&
+        filteredNavigationItems.map((item) => (
+          <Link
+            href={item.to}
+            key={item.to}
+            className={classNames(
+              'flex w-full items-center justify-between gap-2 rounded-lg px-3 py-2 text-sm font-medium transition-colors duration-200',
+              router.pathname === item.to
+                ? 'bg-black text-white'
+                : 'bg-white text-black hover:bg-neutral-100',
+            )}
+          >
+            <div className="flex w-full items-center justify-center gap-2">
+              <item.icon className="w-6" />
+              {t(`sidebar.items.${item.key}`)}
+            </div>
+          </Link>
+        ))}
     </>
   );
 };
