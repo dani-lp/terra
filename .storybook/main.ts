6eeac98b0d9829f3e72c14601079da1e65c2dc06
Dani Lauzurica <danilau11@gmail.com>
2023-04-20 00:36:28 +0200

    fix: deprecated storybook addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,31 +1,28 @@
 const path = require('path');
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", {
-    name: '@storybook/addon-postcss',
-    options: {
-      postcssLoaderOptions: {
-        implementation: require('postcss')
-      }
-    }
-  }, "@storybook/addon-mdx-gfm"],
-  "framework": {
-    name: "@storybook/nextjs",
-    options: {}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-styling',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
-  "core": {
-    "disableTelemetry": true
+  core: {
+    disableTelemetry: true,
   },
-  "webpackFinal": async (config, {
-    configType
-  }) => {
+  webpackFinal: async (config, { configType }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src')
+      '@': path.resolve(__dirname, '../src'),
     };
     return config;
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
