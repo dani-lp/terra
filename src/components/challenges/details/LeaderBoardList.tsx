27e06031dd6dc8d58e6f700ccd9b4c98980ac3fb
Dani Lauzurica <danilau11@gmail.com>
2023-04-27 21:51:01 +0200

    Leaderboard empty state

--- src/components/challenges/details/LeaderBoardList.tsx
+++ src/components/challenges/details/LeaderBoardList.tsx
@@ -5,6 +5,8 @@ import { LeaderBoardListRowSkeleton } from '@/components/challenges/details/Lead
 import { PlayerDetailsSlideOver } from '@/components/users';
 import { classNames } from '@/const';
 import { trpc } from '@/utils/trpc';
+import { UserGroupIcon } from '@heroicons/react/24/outline';
+import { useTranslation } from 'next-i18next';
 
 type Props = {
   loading: boolean;
@@ -17,6 +19,7 @@ export const LeaderBoardList = ({
   challengeId,
   className,
 }: Props) => {
+  const { t } = useTranslation('challenges');
   const { data, isLoading, isError, error } = trpc.participation.getByChallenge.useQuery({
     challengeId,
   });
@@ -34,13 +37,15 @@ export const LeaderBoardList = ({
   return (
     <>
       <ol className={classNames('flex flex-col gap-1', className)}>
-        {loading ? (
+        {loading && (
           <>
             {[...Array(4)].map((_, index) => (
               <LeaderBoardListRowSkeleton key={index} position={index + 1} />
             ))}
           </>
-        ) : (
+        )}
+        {!loading &&
+          leaderboardEntries.length > 0 &&
           leaderboardEntries.map((user, index) => (
             <LeaderBoardListRow
               key={user.username}
@@ -51,7 +56,15 @@ export const LeaderBoardList = ({
               score={user.points}
               onClick={() => setOverviewPlayerId(user.playerId)}
             />
-          ))
+          ))}
+        {!loading && leaderboardEntries.length === 0 && (
+          <div className="flex flex-col items-center justify-center pt-10 text-center">
+            <div className="mx-auto rounded-full bg-purple-100 p-2">
+              <UserGroupIcon className="mx-auto h-8 w-8 text-purple-700" />
+            </div>
+            <h3 className="mt-2 text-sm font-semibold text-gray-900">{t('challenges.details.noPlayers')}</h3>
+            <p className="mt-1 text-sm text-gray-500">{t('challenges.details.noPlayersDescription')}</p>
+          </div>
         )}
       </ol>
       <PlayerDetailsSlideOver
