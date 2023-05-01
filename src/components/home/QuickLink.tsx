a1a152ded896c6ae312bee416dc624ac6f15403e
Dani Lauzurica <danilau11@gmail.com>
2023-05-01 18:20:45 +0200

    Quick links for players

---
+++ src/components/home/QuickLink.tsx
@@ -0,0 +1,41 @@
+import { classNames } from '@/const';
+import Link from 'next/link';
+
+export type QuickLinkItem = {
+  title: string;
+  description: string;
+  icon: React.ComponentType<React.ComponentProps<'svg'>>;
+  background: string;
+  href: string;
+};
+
+type Props = {
+  item: QuickLinkItem;
+};
+
+export const QuickLink = ({ item }: Props) => {
+  return (
+    <Link href={item.href}>
+      <li className="flow-root">
+        <div className="relative -m-2 flex items-center space-x-4 rounded-xl p-2 focus-within:ring-2 focus-within:ring-indigo-500 hover:bg-gray-50">
+          <div
+            className={classNames(
+              item.background,
+              'flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-lg',
+            )}
+          >
+            <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
+          </div>
+          <div>
+            <h3 className="text-sm font-medium text-gray-900">
+              <span className="absolute inset-0" aria-hidden="true" />
+              <span>{item.title}</span>
+              <span aria-hidden="true"> &rarr;</span>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+          </div>
+        </div>
+      </li>
+    </Link>
+  );
+};
--- src/components/home/players/PlayerQuickLinksCard.tsx
+++ src/components/home/players/PlayerQuickLinksCard.tsx
@@ -1,12 +1,42 @@
 import { HomeCard } from '@/components/home/HomeCard';
+import { QuickLink, type QuickLinkItem } from '@/components/home/QuickLink';
+import { InformationCircleIcon, RocketLaunchIcon, TrophyIcon } from '@heroicons/react/24/outline';
 import { useTranslation } from 'next-i18next';
 
 export const PlayerQuickLinksCard = () => {
   const { t } = useTranslation('home');
 
+  const links: QuickLinkItem[] = [
+    {
+      title: t('players.quickLinks.availableChallenges'),
+      description: t('players.quickLinks.availableChallengesMsg'),
+      icon: RocketLaunchIcon,
+      background: 'bg-indigo-500',
+      href: '/challenges?challengesTab=available',
+    },
+    {
+      title: t('players.quickLinks.activeChallenges'),
+      description: t('players.quickLinks.activeChallengesMsg'),
+      icon: TrophyIcon,
+      background: 'bg-green-500',
+      href: '/challenges?challengesTab=joined',
+    },
+    {
+      title: t('players.quickLinks.about'),
+      description: t('players.quickLinks.aboutMsg'),
+      icon: InformationCircleIcon,
+      background: 'bg-yellow-500',
+      href: '/about',
+    },
+  ];
+
   return (
     <HomeCard title={t('players.quickLinks.title')} subtitle={t('players.quickLinks.subtitle')}>
-      test
+      <ul role="list" className="grid grid-cols-1 gap-6 py-6 sm:grid-cols-2">
+        {links.map((item, i) => (
+          <QuickLink item={item} key={i} />
+        ))}
+      </ul>
     </HomeCard>
   );
 };
